87650712f5da83f78cb2749c1660bb78ce9da88b
Jules Gorce <jules.gorce@gmail.com>
2022-04-08 11:36:25 +0200

    first commit : new app with cable ready

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  include CableReady::Broadcaster
+  def index
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.user = current_user
+    post.save!
+    cable_ready["timeline"].insert_adjacent_html(
+      selector: "#timeline",
+      position: "afterbegin",
+      html: render_to_string(partial: "post", locals: { post: post })
+    )
+    cable_ready.broadcast
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_084136) do
+ActiveRecord::Schema.define(version: 2022_04_08_093200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "body", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "reactions", force: :cascade do |t|
+    t.string "emoji"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_reactions_on_post_id"
+    t.index ["user_id"], name: "index_reactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +45,7 @@ ActiveRecord::Schema.define(version: 2022_04_08_084136) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
+  add_foreign_key "reactions", "posts"
+  add_foreign_key "reactions", "users"
 end
